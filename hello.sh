d5ad9b6757e20b2b7ef4d8d82291d49870daee98
howard <howardsoon@gmail.com>
2022-03-25 16:38:17 +0000

    deleted images

--- hello.sh
+++ hello.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 echo hello world!
-<<<<<<< HEAD
-
-<img src="sunset.jpg">
 
 # Add new image: sunset.jpg
 
 # this is a new line
-=======
+
 <div><img src="img_hello_world.jpg" alt="Hello World from Space"
 style="width:100%;max-width:960px"></div>
 >>>>>>> hello-world-images
